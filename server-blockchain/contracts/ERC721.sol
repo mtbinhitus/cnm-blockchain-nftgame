5b80dbdd3f3e4d5cc45aaaa17ac5ff58d57d95fa
Binh Mai <mtbinhitus@gmail.com>
2023-08-17 22:32:18 +0700

    complete ERC721

--- server-blockchain/contracts/ERC721.sol
+++ server-blockchain/contracts/ERC721.sol
@@ -1,17 +1,17 @@
 pragma solidity ^0.8.2;
 
 contract ERC721 {
-    // event Transfer(
-    //     address indexed _from,
-    //     address indexed _to,
-    //     uint256 indexed _tokenId
-    // );
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId
+    );
 
-    // event Approval(
-    //     address indexed _owner,
-    //     address indexed _approved,
-    //     uint256 indexed _tokenId
-    // );
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 indexed _tokenId
+    );
 
     event ApprovalForAll(
         address indexed _owner,
@@ -19,32 +19,10 @@ contract ERC721 {
         bool _approved
     );
 
-    // function safeTransferFrom(
-    //     address _from,
-    //     address _to,
-    //     uint256 _tokenId,
-    //     bytes data
-    // ) external payable;
-
-    // function safeTransferFrom(
-    //     address _from,
-    //     address _to,
-    //     uint256 _tokenId
-    // ) external payable;
-
-    // function transferFrom(
-    //     address _from,
-    //     address _to,
-    //     uint256 _tokenId
-    // ) external payable;
-
-    // function approve(address _approved, uint256 _tokenId) external payable;
-
-    // function getApproved(uint256 _tokenId) external view returns (address);
-
     mapping(address => uint256) internal _balances;
     mapping(uint256 => address) internal _owners;
-    mapping(address => mapping(address => bool)) private _operatorApprivals;
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+    mapping(uint256 => address) private _tokenApprovals;
 
     //return number of NFTs of an user
     function balanceOf(address owner) external view returns (uint256) {
@@ -53,7 +31,7 @@ contract ERC721 {
     }
 
     //Find owner of an NFT
-    function ownerOf(uint256 tokenId) external view returns (address) {
+    function ownerOf(uint256 tokenId) public view returns (address) {
         address owner = _owners[tokenId];
         require(owner != address(0), "Token Id is not exits"); // check for standard ERC721
 
@@ -62,7 +40,7 @@ contract ERC721 {
 
     // enable or disable an operator
     function setApprovalForAll(address operator, bool approved) external {
-        _operatorApprivals[msg.sender][operator] = approved;
+        _operatorApprovals[msg.sender][operator] = approved;
         emit ApprovalForAll(msg.sender, operator, approved);
     }
 
@@ -70,8 +48,88 @@ contract ERC721 {
     function isApprovedForAll(
         address owner,
         address operator
-    ) external view returns (bool) {
+    ) public view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+    // Updates an approved address for an NFT
+    function approve(address to, uint256 tokenId) public payable {
+        address owner = ownerOf(tokenId);
+        require(
+            msg.sender == owner || isApprovedForAll(owner, msg.sender),
+            "msg.sender is not owner approval operator"
+        );
 
-        return _operatorApprivals[owner][operator];
+        _tokenApprovals[tokenId] = to;
+        emit Approval(owner, to, tokenId);
+    }
+
+    function getApproved(uint256 tokenId) public view returns (address) {
+        require(_owners[tokenId] != address(0), "Token id does not exits"); // check for standard ERC721
+
+        return _tokenApprovals[tokenId];
+    }
+
+    // transfer NFT to 
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public payable {
+        address owner = ownerOf(tokenId);
+
+        require(
+            msg.sender == owner || 
+            getApproved(tokenId) == msg.sender ||
+            isApprovedForAll(owner, msg.sender), "Msg.sender is not the owner..."
+        );
+
+        require(owner == from , "from address is not the owner");
+
+        require(to != address(0), "Address is the zero");
+
+        require(_owners[tokenId] != address(0), "Token id does not exits"); // check for standard ERC721
+
+
+        approve((address(0)), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+
+        //update
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    // standard transferFrom method
+    // check if the receiver smart contract is capable of receiving NFT
+     function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public payable {
+        transferFrom(from, to, tokenId);
+        require(_checkOnERC721Received(), "Reciver not implemented");
+
+    }
+
+    // simple version to check for NFT receivability of a smart contract
+    function _checkOnERC721Received() private pure returns(bool) {
+        return true;
+    }
+
+    // 
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external payable {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    // check implement when deploy in marketplace - EIP165 proposal
+    function supportInterface(bytes4 interfaceID) public pure virtual returns(bool) {
+        return interfaceID == 0x80ac58cd;
     }
 }
