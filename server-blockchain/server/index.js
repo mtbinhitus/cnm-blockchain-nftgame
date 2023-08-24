07a0a9ebf66c2f582ff0795eca56c65fa0da37ed
longproxd <longproxd1@gmail.com>
2023-08-24 19:17:29 +0700

    send NFT to other wallet

--- server-blockchain/server/index.js
+++ server-blockchain/server/index.js
@@ -1,14 +1,17 @@
-const API_KEY = "7u9ag44gEfuJtfihRfFlgTU4UMEV02HV";
 const PRIVATE_KEY =
   "dea7d2820b27c8ea5e7c9e54ca2a6365607373649e9de92752d8cf844559b5ad";
+
 const CONTRACT_ADDRESS = "0x404f79c45D27512D05C1EBfdBA6FF4B3Ffc6b6e5";
+
 const API_URL =
   "https://eth-goerli.g.alchemy.com/v2/7u9ag44gEfuJtfihRfFlgTU4UMEV02HV";
+
 const ACCOUNT_ADDRESS = "0xA6fae02810B4CCA54d7D3219c4878905A5FFdC22";
 
-
-
 const fs = require("fs");
+
+const Web3 = require("web3");
+
 
 const contractJson = fs.readFileSync(
   "./build/artifacts/contracts/TrophiesCard.sol/TrophiesCard.json",
@@ -19,8 +22,6 @@ const contractData = JSON.parse(contractJson);
 
 const contractAbi = contractData.abi;
 
-
-const Web3 = require("web3");
 const web3 = new Web3(API_URL);
 
 const contract = new web3.eth.Contract(contractAbi, CONTRACT_ADDRESS);
@@ -53,7 +54,41 @@ const main = async () => {
   console.log("Transaction receipt:", txReceipt);
 };
 
-main();
+const senNFT = async (token_id_nft, address_to) => {
+
+  const gasPrice = await web3.eth.getGasPrice();
+  const nonce = await web3.eth.getTransactionCount(ACCOUNT_ADDRESS);
+
+  const txObject = {
+    from: ACCOUNT_ADDRESS,
+    to: CONTRACT_ADDRESS,
+    gas: 210000, // Số gas bạn muốn sử dụng
+    gasPrice: gasPrice,
+    nonce: nonce,
+    data: contract.methods
+      .transferFrom(ACCOUNT_ADDRESS, address_to, token_id_nft)
+      .encodeABI(), // Gọi hàm transferFrom() để chuyển NFT
+  };
+
+  // Ký giao dịch với private key
+  const signedTx = await web3.eth.accounts.signTransaction(
+    txObject,
+    PRIVATE_KEY
+  );
+
+  // Gửi giao dịch đã ký
+  const txReceipt = await web3.eth.sendSignedTransaction(
+    signedTx.rawTransaction
+  );
+
+  console.log("Transaction receipt:", txReceipt);
+};
+
+// main();
+
+senNFT(1, "0x33739B1802B981D04c2c6BcD1637963438dc057E");
+
+
 // .then(() => process.exit(0)).catch(err => {
 //     console.log(err)
 // })
