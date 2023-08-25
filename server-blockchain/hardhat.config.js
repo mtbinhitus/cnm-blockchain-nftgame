2a440de48829d74e3db308e178376f23a7463c98
Thinh <thinhluongit@gmail.com>
2023-08-26 01:15:17 +0700

    ok

--- server-blockchain/hardhat.config.js
+++ server-blockchain/hardhat.config.js
@@ -14,17 +14,17 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   defaultNetwork: 'hardhat',
-  // networks: {
-  //   hardhat: {
-  //     chainId: 31337,
-  //   },
-  //   goerli: {
-  //     url: GOERLI_RPC_URL,
-  //     accounts: [PRIVATE_KEY],
-  //     chainId: 5,
-  //     blockConfirmations: 5,
-  //   },
-  // },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 5,
+    },
+  },
   solidity: {
     compilers: [
       {
